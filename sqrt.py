6b858cdea2610d203a817d6b78b6d2bf129190c8
Harrison Li <hli76@student.ubc.ca>
2025-11-17 11:50:29 -0800

    code style fix

--- sqrt.py
+++ sqrt.py
@@ -6,13 +6,14 @@
 import click
 import math
 
+
 @click.command()
 @click.option("--n", type=int, required=True, help="Number for which the square root should be calculated")
-
 def main(n):
     if n < 0:
         raise Exception("n should not a positive number")
     print(math.sqrt(n))
+
 
 if __name__ == "__main__":
     main()
